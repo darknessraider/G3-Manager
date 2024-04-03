9d0b31c168be0caadf7f96f1c2f3f11acb4646fd
Woody Spivey <woodyspivey@gmail.com>
2024-04-03 13:06:12 -0400

    get templates working, static files not working yet

--- g3manager/g3manager/settings.py
+++ g3manager/g3manager/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'g3manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
