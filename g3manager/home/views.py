7a108087270ee9830ec562e09eaaa4c6f0b47e54
Woody Spivey <woodyspivey@gmail.com>
2024-04-03 23:37:03 -0400

    add part display listing

--- g3manager/home/views.py
+++ g3manager/home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from partmanager.models import Part
 
 # Create your views here.
 
 def index(request):
-    return render(request, "homepage.html")
+    return render(request, "homepage.html", {'parts': Part.objects.all()})
